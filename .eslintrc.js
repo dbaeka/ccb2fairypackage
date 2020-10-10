0bb498e1c106eacc773a93ef1d2388db93169557
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2020-10-10 02:38:05 +0000

    updated config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
-printWidth: 120
-tabWidth: 4
-singleQuote: true
+module.exports = {
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'no-console': 'error',
+        curly: ['error', 'all'],
+        'prefer-arrow-callback': 'error',
+        'one-var': ['error', 'never'],
+        'no-var': 'error',
+        'prefer-const': 'error'
+    },
+    parserOptions: {
+        ecmaVersion: 6
+    }
+};
